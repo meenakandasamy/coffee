d38bab68606add087d5681a69eabe67ba829748e
meenakandasamy <128149997+meenakandasamy@users.noreply.github.com>
2023-11-06 10:20:38 +0530

    ..

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./Home/Home";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          </Routes>
+          </Router>
+    </div>
+  );
+}
+
+export default App;
